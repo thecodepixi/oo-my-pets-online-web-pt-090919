0374924680ad808390edb86663cfc51b3bbc496f
Emily Harber <emily.a.harber@gmail.com>
2019-10-12 15:28:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,20 @@
 class Owner
   # code goes here
   attr_reader :name, :species 
+  @@all = []
   
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end 
   
   def say_species
     "I am a #{@species}." 
   end 
+  
+  def self.all 
+    @@all 
+  end 
+  
 end
